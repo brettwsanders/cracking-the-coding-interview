47fdfea32561388333702085c36b259066a2a6e5
Brett Sanders <bsand08@gmail.com>
2016-01-19 19:22:34 -0800

    add hints

--- Ch-1-Arrays-and-Strings/is_unique.js
+++ Ch-1-Arrays-and-Strings/is_unique.js
@@ -1,6 +1,14 @@
+//1.1
 //Is Unique
+//pg. 90
+
 //Implement an algorithm to determine if a string has all unique characters.
 //What if you cannot use additional data structures?
+
+//Hints
+//1. Try a hash table
+//2. Could a bit vector be useful?
+//3. Can you solve it in O(N log N) time? What might a solution like that look like?
 
 function isUnique(string) {
   var chars = {};
@@ -13,10 +21,10 @@ function isUnique(string) {
   return true;
 }
 
-// function isUniqueAlt(string) {
-//   for (var i = 0; i < string.length; i++) {
-//     for (var j = 0; j < string.length; j++) {
-      
-//     }
-//   }
-// }
+function isUniqueAlt(string) {
+  for (var i = 0; i < string.length; i++) {
+    for (var j = 0; j < string.length; j++) {
+
+    }
+  }
+}
